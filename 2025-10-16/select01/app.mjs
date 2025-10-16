05a331aa73a205b367187304930583361f49d358
borossandor27 <boros.sanyi@gmail.com>
2025-10-16 13:40:56 +0200

    1.33

--- 2025-10-16/select01/app.mjs
+++ 2025-10-16/select01/app.mjs
@@ -2,3 +2,6 @@ import gyumolcsok from './gyumolcsok.json' with { type: 'json' };
 console.log(gyumolcsok);
 let rendezett_gyumolcsok = [...gyumolcsok].sort((a,b)=> a.ar - b.ar);
 console.log(rendezett_gyumolcsok);
+console.log("Készeleten lévő gyümölcsök:");
+let keszleten_gyumolcsok = rendezett_gyumolcsok.filter(e => e.keszleten).map(gyumolcs => gyumolcs.neve);
+console.log(keszleten_gyumolcsok);
