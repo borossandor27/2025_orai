99fa553ea3f5c30eaa4541314bd2b67f52a0607e
borossandor27 <boros.sanyi@gmail.com>
2025-11-28 15:36:39 +0100

    1.53

--- felhasznalok/src/App.jsx
+++ felhasznalok/src/App.jsx
@@ -1,25 +1,36 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Felhasznalokartya from './compononents/Felhasznalokartya';
 
 function App() {
   const [users, setUsers] = useState([]);
   let backendUrl = "https://randomuser.me/api/?results=10";
 
-  useEffect(async () => {
-    try {
-      let results = await axios.get(backendUrl);
-      console.log(results.data.results);
-      setUsers(results.data.results);
-    } catch (error) {
-      console.error("Error fetching users:", error);
-    }
+  useEffect(() => {
+    const felhasznalokBetoltese = async () => {
+      try {
+        let results = await axios.get(backendUrl);
+        console.log(results.data.results);
+        setUsers(results.data.results);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    felhasznalokBetoltese();
   }, []);
 
 
   return (
     <>
       <h1>Felhasználók listája</h1>
-
+      {users.map((user, index) => (
+        <Felhasznalokartya
+          key={index}
+          name={user.name}
+          email={user.email}
+          picture={user.picture}
+        />
+      ))}
     </>
   )
 }
--- felhasznalok/src/compononents/Felhasznalokartya.jsx
+++ felhasznalok/src/compononents/Felhasznalokartya.jsx
@@ -1,9 +1,11 @@
-const Felhasznalokartya = ({name, email, picture}) => {
+const Felhasznalokartya = ({ name, email, picture }) => {
     return (
-        <div>
+        <div className="felhasznalo-kartya">
             <img src={picture.large} alt={`${name.first} ${name.last}`} />
-            <h2>{name.title} {name.first} {name.last}</h2>
-            <p>{email}</p>
+            <div className="felhasznalo-adatai">
+                <h2>{name.title} {name.first} {name.last}</h2>
+                <p>{email}</p>
+            </div>
         </div>
     );
 };
