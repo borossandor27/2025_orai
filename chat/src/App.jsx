cc2fb7a1033c65ac8d88a9d7f021b0dda4e81df5
borossandor27 <boros.sanyi@gmail.com>
2025-11-28 10:59:34 +0100

    1.51

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,9 +1,9 @@
 import Cikk from "./components/Cikk"
 function App() {
   const cikkek = [
-    { id: 1, title: "első", content: "ez az első cikk" },
-    { id: 2, title: "második", content: "ez a második cikk" },
-    { id: 3, title: "harmadik", content: "ez a harmadik cikk" },
+    { id: 1, title: "első", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 2, title: "második", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { id: 3, title: "harmadik", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
   ]
   return (
     <>
