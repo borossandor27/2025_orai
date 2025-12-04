4d5ddf3f5c86616bace98b56820a78d13b00421a
borossandor27 <boros.sanyi@gmail.com>
2025-12-04 12:18:12 +0100

    1.541

--- 12-04/updown/src/App.jsx
+++ 12-04/updown/src/App.jsx
@@ -10,27 +10,27 @@ function App() {
     console.log("useEffect futott");
     // eseménykezelő függvények létrehozása
     const handleUpClick = () => setCount((count) => count + 1);
-    const handleDownClick = () => setCount((count) => count - 1);
-    // eseménykezelők hozzárendelése a gombokhoz
-    const upButton = document.querySelector('.up');
-    const downButton = document.querySelector('.down');
-    upButton.addEventListener('click', handleUpClick);
-    downButton.addEventListener('click', handleDownClick);
-    return () => {
-      upButton.removeEventListener('click', handleUpClick);
-      downButton.removeEventListener('click', handleDownClick);
-    };
-  }, []);
+    const handleDownClick = () => setCount((count) => (count > 0 ? count - 1 : 0));
+  // eseménykezelők hozzárendelése a gombokhoz
+  const upButton = document.querySelector('.up');
+  const downButton = document.querySelector('.down');
+  upButton.addEventListener('click', handleUpClick);
+  downButton.addEventListener('click', handleDownClick);
+  return () => {
+    upButton.removeEventListener('click', handleUpClick);
+    downButton.removeEventListener('click', handleDownClick);
+  };
+}, []);
 
-  return (
-    <>
-      <h1>Egyszerű számláló</h1>
-      <div id="kezeloPanel">
-        <button className="up">+</button>
-        <p>{count}</p>
-        <button className="down">-</button>
-      </div>
-    </>
-  )
+return (
+  <>
+    <h1>Egyszerű számláló</h1>
+    <div id="kezeloPanel">
+      <button className="up">+</button>
+      <p>{count}</p>
+      <button className="down">-</button>
+    </div>
+  </>
+)
 }
 export default App
