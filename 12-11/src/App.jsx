056729b711c812ee9b71f9f36a302cffad8beb25
borossandor27 <boros.sanyi@gmail.com>
2025-12-11 14:58:21 +0100

    1.56

--- 12-11/src/App.jsx
+++ 12-11/src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LoadData from './components/LoadData'
+import KeresoPanel from './components/KeresoPanel'
+import DataTable from './components/DataTable'
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [adatok, setAdatok] = useState([])
+  const [filters, setFilters] = useState({
+    id: '',
+    nev: '',
+    kor: '',
+    belepesDatuma: ''
+  })
+  const [szurtAdatok, setSzurtAdatok] = useState(adatok)
+
+  const szures = (e) => {
+    e.preventDefault() // Alapértelmezett űrlap feldolgozásának megakadályozása
+    console.log('Szűrési feltételek:', filters);
+    let szurtAdat = adatok.filter((adatsor) => {
+      return (
+        (filters.id === '' || adatsor.id === Number(filters.id)) &&
+        (filters.nev === '' || adatsor.TeljesNev.toLowerCase().includes(filters.nev.toLowerCase())) &&
+        (filters.kor === '' || adatsor.Eletkor === Number(filters.kor)) &&
+        (filters.belepesDatuma === '' || new Date(adatsor.BelepesDatuma).toLocaleDateString() === new Date(filters.belepesDatuma).toLocaleDateString())
+      )
+    })
+    setSzurtAdatok(szurtAdat)
+    }
+
+  useEffect(() => {
+    LoadData({ adatok, setAdatok });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Keresés adatokban</h1>
+      <form action="">
+        <button onClick={szures}>Keresés</button>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Név</th>
+              <th>Kor</th>
+              <th>Belépés dátuma</th>
+            </tr>
+            <KeresoPanel filters={filters} setFilters={setFilters} />
+          </thead>
+          <DataTable adatok={szurtAdatok} />
+        </table>
+      </form>
     </>
   )
 }
