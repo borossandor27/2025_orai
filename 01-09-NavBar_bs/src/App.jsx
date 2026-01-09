088c6f6c941c4a1f09ddb5b50e9d1652f2631715
borossandor27 <boros.sanyi@gmail.com>
2026-01-09 10:35:15 +0100

    1.71

--- 01-09-NavBar_bs/src/App.jsx
+++ 01-09-NavBar_bs/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './layout/Navbar'
+import Main from './layout/Main'
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
- 
+
 
   return (
-    <>
-     
-    </>
+
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </BrowserRouter>
+
   )
 }
 
---
+++ 01-09-NavBar_bs/src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+const Contact = () => {
+    return (
+        <div className="container mt-4">
+            <h1>Contact Us</h1>
+        </div>
+    );
+}
+export default Contact;
