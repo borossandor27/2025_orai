6ae54ad70157075100e888a31739fc5804c2ac85
borossandor27 <boros.sanyi@gmail.com>
2026-01-09 09:20:46 +0100

    1.7

---
+++ 01-09-NavBar_bs/README.md
@@ -0,0 +1,11 @@
+# React routing bootstrap-el
+
+Három lap között (`Home`, `Contact` és `About`) kell majd váltani
+
+## Létrehozás
+
+```javascript
+npm create vite@latest myproject
+cd myproject
+npm i react-router-dom bootstrap react-bootstrap
+```
---
+++ 01-09-NavBar_bs/src/layout/Navbar.jsx
@@ -0,0 +1,20 @@
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/about" className="nav-link">About</NavLink>   
+                </li>               
+                 <li className="nav-item">
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>   
+                </li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
