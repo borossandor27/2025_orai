ac9a736a7e8afdc8fc05c5fbd9d8367acca6ab5a
borossandor27 <boros.sanyi@gmail.com>
2026-01-30 07:20:31 +0100

    1.8

---
+++ backend/WindowsFormsApp1/Form_main.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_diakok = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_TeljesNev = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox_Diak = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Diak)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox_diakok
+            // 
+            this.listBox_diakok.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox_diakok.FormattingEnabled = true;
+            this.listBox_diakok.ItemHeight = 20;
+            this.listBox_diakok.Location = new System.Drawing.Point(0, 0);
+            this.listBox_diakok.Name = "listBox_diakok";
+            this.listBox_diakok.Size = new System.Drawing.Size(224, 450);
+            this.listBox_diakok.TabIndex = 0;
+            this.listBox_diakok.SelectedIndexChanged += new System.EventHandler(this.listBox_diakok_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.textBox_Email);
+            this.groupBox1.Controls.Add(this.textBox_TeljesNev);
+            this.groupBox1.Controls.Add(this.textBox_id);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(224, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(576, 450);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kiválasztott diák";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(35, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Location = new System.Drawing.Point(35, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Teljes név";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.Location = new System.Drawing.Point(35, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email cím";
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(175, 55);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.ReadOnly = true;
+            this.textBox_id.Size = new System.Drawing.Size(100, 26);
+            this.textBox_id.TabIndex = 1;
+            // 
+            // textBox_TeljesNev
+            // 
+            this.textBox_TeljesNev.Location = new System.Drawing.Point(175, 96);
+            this.textBox_TeljesNev.Name = "textBox_TeljesNev";
+            this.textBox_TeljesNev.Size = new System.Drawing.Size(208, 26);
+            this.textBox_TeljesNev.TabIndex = 2;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Location = new System.Drawing.Point(175, 143);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(208, 26);
+            this.textBox_Email.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox_Diak);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(573, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 425);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox_Diak
+            // 
+            this.pictureBox_Diak.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox_Diak.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_Diak.Name = "pictureBox_Diak";
+            this.pictureBox_Diak.Size = new System.Drawing.Size(0, 50);
+            this.pictureBox_Diak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_Diak.TabIndex = 0;
+            this.pictureBox_Diak.TabStop = false;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox_diakok);
+            this.Name = "Form_Main";
+            this.Text = "Adatelérés minta";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Diak)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_diakok;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.TextBox textBox_TeljesNev;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox_Diak;
+    }
+}
+
---
+++ backend/WindowsFormsApp1/Form_main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net.Http;
+using Newtonsoft.Json;
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form_Main : Form
+    {
+        string apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+        HttpClient httpClient = new HttpClient();
+        BindingList<Diak> diakok = new BindingList<Diak>();
+        public Form_Main()
+        {
+            InitializeComponent();
+            //MessageBox.Show("konstruktor");
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            
+            getAllData();
+        }
+
+        async Task getAllData()
+        {
+            try
+            {
+                diakok.Clear();
+                var response = await httpClient.GetAsync(apiUrl);
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("API hiba: " + response.StatusCode);
+                    return;
+                }
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var adatok = Diak.FromJson(jsonString);
+
+                foreach (Diak d in adatok)
+                {
+                    diakok.Add(d);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba az adatok lekérése során: " + ex.Message);
+
+            }
+            listBox_diakok.DataSource = diakok;
+        }
+
+        private void listBox_diakok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox_diakok.SelectedIndex >= 0)
+            {
+                Diak selectedDiak = (Diak)listBox_diakok.SelectedItem;
+                textBox_id.Text = selectedDiak.Id.ToString();
+                textBox_TeljesNev.Text = selectedDiak.TeljesNev;
+                textBox_Email.Text = selectedDiak.Email;
+                pictureBox_Diak.ImageLocation = selectedDiak.ImageUrl;
+            }
+        }
+    }
+}
