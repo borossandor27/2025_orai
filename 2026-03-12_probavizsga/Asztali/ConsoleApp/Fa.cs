7aefa8ad4adbf59b6f191f62ee58260842b0c9c1
borossandor27 <boros.sanyi@gmail.com>
2026-03-17 08:13:16 +0100

    2.02

---
+++ 2026-03-12_probavizsga/Asztali/ConsoleApp/Fa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Fa
+    {
+        public int Azon { get; set; }
+        public string Faj { get; set; }
+        public int Kormeret { get; set; }
+        public int TelepulesId { get; set; }
+        public int MeresEve { get; set; }
+        public string TelepulesNev { get; set; }
+
+        public Fa(string sor)
+        {
+            // "azon","faj","kormeret","telepules_id","meres"
+            string[] adatok = sor.Replace("\"", "").Split(';');
+            Azon = int.Parse(adatok[0]);
+            Faj = adatok[1];
+            Kormeret = int.Parse(adatok[2]);
+
+            TelepulesId = int.Parse(adatok[3]);
+            MeresEve = int.Parse(adatok[4]);
+            TelepulesNev = string.Empty;
+
+        }
+
+    }
+}
---
+++ 2026-03-12_probavizsga/Asztali/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _telepulesekBeolvasasa("telepules.csv");
+            Console.WriteLine();
+        }
+
+        private static void _telepulesekBeolvasasa(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
